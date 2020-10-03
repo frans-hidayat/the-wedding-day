8b672539362bd7d1036464ab51b49fca8439e9d6
frans-hidayat <48579779+frans-hidayat@users.noreply.github.com>
2020-10-03 09:48:05 +0700

    Update main.js
    Update date

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Feb 23, 2021 15:00:00").getTime();
+		var countDownDate = new Date("Nov 20, 2021 10:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
